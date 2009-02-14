c232ca887a62a3569565abcb13b9e9312d4a7909
1000copy <1000copy@6152ed1a-f529-11dd-9d1a-b59b2e1864b6>
2009-02-14 06:21:40 +0000

    比较完美了。需要计划他的使用性测试（AcceptTest)了。 比如更多的sql的支持是否可以？ 或者做一个项目?

--- Program.cs
+++ Program.cs
@@ -23,8 +23,7 @@ namespace SqlSmartTest
                 SSSelect();
                 SSJoin();
                 SSCount();
-                //SqliteConnTest();
-               
+                //TestSqlite.SqliteConnTest();
             }
             catch (Exception ex)
             {
@@ -45,7 +44,7 @@ namespace SqlSmartTest
         private static void SSJoin()
         {
             QueryPersons persons = new QueryPersons();
-            persons.Exec();
+            persons.DoQuery();
             foreach (QueryPerson person in persons)
             {
                 string str = string.Format("id= {0},name={1},deptname={2}", person.Id.Value, person.Name.Value,person.DeptName.Value);
@@ -55,7 +54,7 @@ namespace SqlSmartTest
         private static void SSSelect()
         {
             PersonList persons = new PersonList();
-            persons.SelectAll();
+            persons.DoQuery();
                            
             foreach (Person person in persons)
             {
@@ -64,7 +63,7 @@ namespace SqlSmartTest
             }
        
             DeptList depts = new DeptList();
-            depts.SelectAll();
+            depts.DoQuery();
             foreach (Dept dept in depts)
             {
                 string str = string.Format("id= {0},name={1}", dept.Id.Value, dept.Name.Value);
@@ -88,72 +87,5 @@ namespace SqlSmartTest
             companydb.Person.DeleteAll();
             companydb.Dept.DeleteAll();
         }
-
-        /*
-       private static void SqliteConnTest()
-       {
-           try
-           {
-               DbHelper db = new DbHelper("Data Source=companydb.db");
-               db.Exec("INSERT INTO person(id,name) VALUES (1,'1000copy')");
-               DbDataReader reader = db.QueryReader("SELECT ID, name FROM person");
-               if (reader.HasRows)
-               {
-                   while (reader.Read())
-                   {
-                       Console.WriteLine("ID: " + reader.GetInt16(0));
-                       Console.WriteLine("name: " + reader.GetString(1));
-                   }
-               }
-           }
-           catch
-           {
-               Console.Out.WriteLine("some error");
-           }
-           Console.In.ReadLine();
-       }
-
-       private static void SSTest()
-       {
-
-           MyCompanyDb db = new MyCompanyDb();
-           string sql = "";
-           sql = db.Dept.SelectAll();
-           Console.Out.WriteLine(sql);
-           // Insert
-           db.Dept.Id.Value = 1;
-           db.Dept.Name.Value = "trd";
-           sql = db.Dept.Insert();
-           // update 
-           Console.Out.WriteLine(sql);
-           sql = db.Dept.Update();
-           Console.Out.WriteLine(sql);
-           // delete
-           sql = db.Dept.Delete();
-           Console.Out.WriteLine(sql);
-           // left join sql 
-           sql = db.PersonList();
-           Console.Out.WriteLine(sql);
-           // insert person
-           db.Person.Birthday.Value = DateTime.Today.Date;
-           db.Person.DeptId.Value = 1;
-           db.Person.Name.Value = "1000copy";
-           db.Person.Id.Value = 1;
-           sql = db.Person.Insert();
-           Console.Out.WriteLine(sql);
-           // pause ,wait for user input 
-           Console.In.ReadLine();
-       }
-       
-       public class MyCompanyDb : CompanyDb
-       {
-           public string PersonList()
-           {
-               string sql = "";
-               sql = "select {0} from {1} left join {2} on {3} ={4} ";
-               sql = string.Format(sql, Dept.Name + "," + Person.FieldNamesPrefixTableName, Person, Dept, Person.DeptId, Dept.Id);
-               return sql;
-           }
-       }*/
     }
 }
